0258f7819a9778ad14e70390c57052605fa7cd1f
Ben Brumby <bbrumby00@gmail.com>
2020-10-29 20:37:10 -0400

    npm installs

--- routes/view.js
+++ routes/view.js
@@ -1,1 +1,7 @@
 //serve up HTML files
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
