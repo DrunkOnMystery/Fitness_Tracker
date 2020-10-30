01d760c8f31a94c8e13e23c9e76452f1c4589e7a
Ben Brumby <bbrumby00@gmail.com>
2020-10-30 09:58:59 -0400

    deleted router path that was wrong

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,6 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-router.Workout.create({ name: "Workouts" })
-    .then(dbLibrary => {
-        console.log(dbLibrary);
-    })
-    .catch(({ message }) => {
-        console.log(message);
-    });
-
 router.post("/api/workouts", (req, res) => {
     Workout.create(req.body)
         .then(dbWorkout => {
@@ -20,7 +12,7 @@ router.post("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts", (req, res) => {
-    Workout.find({}).sort({ _id: -1 }).limit(1)
+    Workout.find({})
         .populate("workouts")
         .then(dbWorkout => {
             res.json(dbWorkout);
